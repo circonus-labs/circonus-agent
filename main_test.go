cf2ef3b15396e54be296cd5da7b460ae64a5a4a1
matt maier <maier@users.noreply.github.com>
2017-09-28 08:58:08 -0400

    test (placeholder)

---
+++ main_test.go
@@ -0,0 +1,12 @@
+// Copyright © 2017 Circonus, Inc. <support@circonus.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	t.Log("Placeholder...nothing to test currently")
+}
