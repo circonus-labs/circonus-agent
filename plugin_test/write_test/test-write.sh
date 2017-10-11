7fd645077d8a5e1bc77f6e88d014bfc0e9491116
matt maier <maier@users.noreply.github.com>
2017-10-11 17:53:02 -0400

    fix write tests - only structured json

--- plugin_test/write_test/test-write.sh
+++ plugin_test/write_test/test-write.sh
@@ -8,13 +8,10 @@ data_dir="${PWD}"
 [[ -f "${data_dir}/types.json" ]] || { >&2 echo "Unable to find 'types.json'"; exit 1; }
 
 # simple PUT
-curl -sSL -X PUT -H 'Content-Type: application/json' -d '{"write_put_simple":1}' 'http://127.0.0.1:2609/write/wt_put'
+curl -sSL -X PUT -H 'Content-Type: application/json' -d '{"write_put_simple":{"_type": "i", "_value": 1}}' 'http://127.0.0.1:2609/write/wt_put'
 
 # simple POST
-curl -sSL -X POST -H 'Content-Type: application/json' -d '{"write_post_simple":1}' 'http://127.0.0.1:2609/write/wt_post'
+curl -sSL -X POST -H 'Content-Type: application/json' -d '{"write_put_simple":{"_type": "i", "_value": 1}}' 'http://127.0.0.1:2609/write/wt_post'
 
 # example with each metric type
 curl -sSL -X PUT -H 'Content-Type: application/json' -d @${data_dir}/types.json 'http://127.0.0.1:2609/write/wt_metric_types'
-
-# example from httprap docs
-curl -sSL -X PUT -H 'Content-Type: application/json' -d @${data_dir}/trap.json 'http://127.0.0.1:2609/write/wt_doc_ex'
