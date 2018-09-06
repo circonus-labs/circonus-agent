7da0d317e525aaf23ad955cf5c48940b8a314330
matt maier <maier@users.noreply.github.com>
2018-09-06 12:41:46 -0400

    upd: basic build on rhel functional

--- package/build.sh
+++ package/build.sh
@@ -10,6 +10,7 @@
 ### rhel:rpmbuild
 ###
 
+set -o xtrace
 set -o errtrace
 set -o errexit
 set -o nounset
@@ -62,10 +63,10 @@ base_repo_url="https://github.com/circonus-labs"
 # plugin_version in build.conf to 'master' and build the package.
 : ${url_plugin_repo:="${base_repo_url}/${plugins_name}"}
 # same caveat as plugin
-: ${url_po_repo}:="${base_repo_url}/${po_name}"
+: ${url_po_repo:="${base_repo_url}/${po_name}"}
 
 : ${dir_install_prefix:="/opt/circonus"}
-: ${dir_install_agent:="${dir_install_prefix}/agent"}
+: ${dir_install_agent:="${dir_install}${dir_install_prefix}/agent"}
 
 #
 # commands used during build/install
@@ -79,9 +80,9 @@ base_repo_url="https://github.com/circonus-labs"
 : ${TAR:="tar"}
 : ${TR:="tr"}
 : ${UNAME:="uname"}
-for cmd in $CURL $GIT $TR $UNAME; do {
-    [[ -z "$(type -P $cmd)" ]] && { echo "unable to find '${cmd}' command in [$PATH]"; exit 1}
-}
+for cmd in $CURL $GIT $GO $TAR $TR $UNAME; do
+    [[ -z "$(type -P $cmd)" ]] && { echo "unable to find '${cmd}' command in [$PATH]"; exit 1; }
+done
 : ${FPM:="/usr/local/bin/fpm"}  # only used by Ubuntu builds
 : ${MAKE:="make"}               # freebsd alters
 : ${RPMBUILD:="rpmbuild"}       # only used by RHEL builds
@@ -115,7 +116,7 @@ case $os_type in
             [[ $DISTRIB_RELEASE =~ ^(14.04|16.04)$ ]] || { echo "unsupported Ubuntu release ($DISTRIB_RELEASE)"; exit 1; }
             os_name="ubuntu.${DISTRIB_RELEASE}"
             package_name="${agent_name}-${agent_version}-1.${os_name}_${os_arch}.deb"
-            [[ -z "$(type -P $FPM)" ]] && { echo "unable to find '${FPM}' command in [$PATH]"; exit 1}
+            [[ -z "$(type -P $FPM)" ]] && { echo "unable to find '${FPM}' command in [$PATH]"; exit 1; }
         else
             echo "unknown/unsupported linux variant '$($UNAME -a)'"
             exit 1
@@ -130,7 +131,7 @@ case $os_type in
     #
     freebsd)
         install_target="install-freebsd"
-        relver=`freebsd-version -u | cut -d'-' -f1`
+        relver=$(freebsd-version -u | cut -d'-' -f1)
         [[ -z $relver ]] && { echo "unsupported FreeBSD release >10 required"; exit 1; }
         os_name="$os_type.$relver"
         package_name="${agent_name}-${agent_version}-1.${os_name}_${os_arch}.tgz"
@@ -142,7 +143,7 @@ case $os_type in
         ;;
 esac
 
-[[ -z "$(type -P $MAKE)" ]] && { echo "unable to find '${MAKE}' command in [$PATH]"; exit 1}
+[[ -z "$(type -P $MAKE)" ]] && { echo "unable to find '${MAKE}' command in [$PATH]"; exit 1; }
 
 [[ -z "$os_name" ]] && { echo "invalid os_name (empty)"; exit 1; }
 [[ -z "$package_name" ]] && { echo "invalid package_name (empty)"; exit 1; }
@@ -153,7 +154,7 @@ if [[ -x /usr/bin/nproc ]]; then
 elif [[ -f /proc/cpuinfo ]]; then
     nproc=$(grep -c ^processor /proc/cpuinfo)
 else
-    nproc=`sysctl -n hw.ncpu`
+    nproc=$(sysctl -n hw.ncpu)
 fi
 let make_jobs="$nproc + ($nproc / 2)" # 1.5x the number of CPUs
 
@@ -208,7 +209,7 @@ fetch_agent_repo() {
     fi
 }
 fetch_agent_package() {
-    local stripped_ver=${agent_ver#v}
+    local stripped_ver=${agent_version#v}
     agent_tgz="${agent_name}_${stripped_ver}_${os_type}_64-bit.tar.gz"
     agent_tgz_url="${url_agent_repo}/releases/download/${agent_version}/$agent_tgz"
     [[ -f $agent_tgz ]] || {
@@ -221,6 +222,7 @@ install_agent() {
     fetch_agent_package
 
     echo "-unpacking $agent_tgz into $dir_install_agent"
+    [[ -d $dir_install_agent ]] || mkdir -p $dir_install_agent
     $TAR -zxf $agent_tgz -C $dir_install_agent
 }
 
@@ -257,7 +259,7 @@ install_plugins() {
     pushd $dir_plugin_build >/dev/null
     $GIT checkout master # ensure on master branch
     [[ $plugin_version == "master" ]] || $GIT checkout tags/$plugin_version
-    $MAKE DEST=$dir_install_agent $install_target
+    $MAKE DESTDIR=$dir_install $install_target
     popd >/dev/null
 }
 
@@ -289,6 +291,7 @@ fetch_protocol_observer_repo() {
     fi
 }
 install_protocol_observer() {
+    local rm_go_mod="n"
     fetch_protocol_observer_repo
 
     pushd $dir_po_build >/dev/null
@@ -297,10 +300,14 @@ install_protocol_observer() {
     #
     # NOTE: protocol_observer is a tool in the wirelatency repository
     #
+    # the following line is for go 1.11 modules (outside of GOPATH)
+    [[ -f go.mod ]] || { echo "module github.com/circonus-labs/wirelatency" > go.mod; rm_go_mod="y"; }
     pushd protocol_observer >/dev/null
     $GO build -o ${dir_install_agent}/sbin/protocol-observerd
-
     popd >/dev/null
+    # the following line is for go 1.11 modules (outside of GOPATH)
+    # prevent 'git pull' failing due to dirty local repo
+    [[ -f go.mod && "$rm_go_mod" == "y" ]] && rm go.mod
     popd >/dev/null
 }
 
