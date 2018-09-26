80c739becaebfc8dc44d03598f1c18ed265f3f8e
matt maier <maier@users.noreply.github.com>
2018-09-26 14:10:37 -0400

    upd: allow building specific targets

--- vbuild.sh
+++ vbuild.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-for dist in c7 c6 u16 u14 fb11; do
+distros="c7 c6 u16 u14 fb11"
+[[ -n "$1" ]] && distros="$1"
+
+for dist in $distros; do
     vagrant up $dist && \
     vagrant ssh $dist --command="cd ~/godev/src/github.com/circonus-labs/circonus-agent/package && ./build.sh" && \
     vagrant halt $dist
