254f2723f259d5e551a4f8b4da4a97184d1167fd
matt maier <maier@users.noreply.github.com>
2018-10-05 15:55:58 -0400

    fix: tweak for unattended fb11 build

--- Vagrantfile
+++ Vagrantfile
@@ -113,7 +113,8 @@ Vagrant.configure('2') do |config|
         fb11.vm.synced_folder '.', agent_src_path, type: 'nfs'
         # mac not set in base box, just needs to be set to something to avoid vagrant errors
         fb11.vm.base_mac = ''
-        fb11.ssh.shell = 'test -x /usr/local/bin/bash && /usr/local/bin/bash -l || sh'
+        #fb11.ssh.shell = 'test -x /usr/local/bin/bash && /usr/local/bin/bash -l || sh'
+        fb11.ssh.shell = 'sh'
         fb11.vm.provider 'virtualbox' do |vb|
             vb.name = 'fb11_circonus-agent'
             vb.customize ['modifyvm', :id, '--memory', '2048']
--- vbuild.sh
+++ vbuild.sh
@@ -6,9 +6,15 @@ distros="c7 c6 u18 u16 u14 fb11"
 [[ -n "$1" ]] && distros="$1"
 
 for dist in $distros; do
+    cmd="cd ~/godev/src/github.com/circonus-labs/circonus-agent/package && ./build.sh"
+    [[ $dist == "fb11" ]] && { tmp="/usr/local/bin/bash -l -c '${cmd}'"; cmd=$tmp; }
     vagrant up $dist && \
-    vagrant ssh $dist --command="cd ~/godev/src/github.com/circonus-labs/circonus-agent/package && ./build.sh" && \
+    vagrant ssh $dist --command="${cmd}" && \
     vagrant halt $dist
+    echo
     echo "Waiting 5 seconds [CTRL-C] to abort builds..."
+    echo
     sleep 5
 done
+echo "copying packages to cosi-examples/server for provisioning"
+cp -v package/publish/circonus-agent-*.{rpm,deb,tgz} ../cosi-examples/server/provision/roles/server/files/packages/.
